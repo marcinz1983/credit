ecfdd9c2e50d4aa16ffac7cd49d285667c27d8e0
Marcin <marcinzielinski1983@gmail.com>
2022-01-23 23:10:23 +0000

    [add] - restTemplate implementation done

---
+++ src/main/java/pl/zadanie/credit/mapper/impl/GetCreditsMapperImpl.java
@@ -0,0 +1,72 @@
+package pl.zadanie.credit.mapper.impl;
+
+import org.springframework.stereotype.Component;
+import pl.zadanie.credit.controller.request.GetCustomersRequest;
+import pl.zadanie.credit.controller.response.GetCreditsResponse;
+import pl.zadanie.credit.controller.response.GetCustomersResponse;
+import pl.zadanie.credit.mapper.GetCreditsMapper;
+import pl.zadanie.credit.model.Credit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+@Component
+public class GetCreditsMapperImpl implements GetCreditsMapper {
+
+
+    @Override
+    public GetCustomersRequest mapFromEntityTuGetCustomerRequest(List<Credit> credits) {
+        List<Integer> customersId = credits.stream()
+                .map(id -> id.getCustomerId())
+                .collect(Collectors.toList());
+        return GetCustomersRequest.builder()
+                .withCustomersIds(customersId)
+                .build();
+    }
+
+    @Override
+    public List<GetCreditsResponse> mapCreditAndCustomerToGetCreditResponse(List<Credit> creditList, GetCustomersResponse customers) {
+
+        List<GetCreditsResponse> creditInProgress1 = customers.getCustomers().stream()
+                .map(value -> new GetCreditsResponse(value.getFirstName(), value.getLastName(), value.getPesel(), null, null, null, value.getCustomerId()))
+                .collect(Collectors.toList());
+
+        List<GetCreditsResponse> creditInProgress2 = creditList.stream()
+                .map(value -> new GetCreditsResponse(null, null, null, value.getCreditName(), value.getValue(), value.getId(), value.getCustomerId()))
+                .collect(Collectors.toList());
+
+        Map<Integer, GetCreditsResponse> credit1 = creditInProgress1.stream().collect(Collectors.toMap(GetCreditsResponse::getCustomerId, Function.identity()));
+        Map<Integer, GetCreditsResponse> credit2 = creditInProgress2.stream().collect(Collectors.toMap(GetCreditsResponse::getCustomerId, Function.identity()));
+
+        credit1.keySet().forEach(key -> credit1.merge(key,
+                credit2.get(key),
+                new BiFunction<GetCreditsResponse, GetCreditsResponse, GetCreditsResponse>() {
+                    @Override
+                    public GetCreditsResponse apply(GetCreditsResponse credit1, GetCreditsResponse credit2) {
+                        credit1.setCreditName(credit2.getCreditName());
+                        credit1.setValue(credit2.getValue());
+                        credit1.setCreditId(credit2.getCreditId());
+                        return credit1;
+                    }
+                }));
+
+        List<GetCreditsResponse> creditsList = new ArrayList<GetCreditsResponse>(credit1.values());
+        return creditsList;
+    }
+
+
+}
+
+//        credit1.keySet().forEach(key -> credit1.merge(key,
+//                credit2.get(key),
+//                (credit1, credit2) -> {
+//                    credit1.setCreditName(credit2.getCreditName());
+//                    credit1.setValue(credit2.getValue());
+//                    credit1.setCreditId(credit2.getCreditId());
+//                    return credit1;
+//                }));
--- src/main/java/pl/zadanie/credit/service/impl/CreditServiceImpl.java
+++ src/main/java/pl/zadanie/credit/service/impl/CreditServiceImpl.java
@@ -9,7 +9,9 @@ import pl.zadanie.credit.controller.response.CreateCustomerResponse;
 import pl.zadanie.credit.controller.response.CreditCreateResponse;
 import pl.zadanie.credit.controller.response.CustomerSearchResponse;
 import pl.zadanie.credit.controller.response.GetCreditsResponse;
+import pl.zadanie.credit.controller.response.GetCustomersResponse;
 import pl.zadanie.credit.mapper.CreateCreditMapper;
+import pl.zadanie.credit.mapper.GetCreditsMapper;
 import pl.zadanie.credit.model.Credit;
 import pl.zadanie.credit.repository.CreditRepository;
 import pl.zadanie.credit.service.CreditService;
@@ -22,25 +24,27 @@ public class CreditServiceImpl implements CreditService {
     private final CreditRepository creditRepository;
     private final CustomerController customerController;
     private final CreateCreditMapper createCreditMapper;
+    private final GetCreditsMapper getCreditsMapper;
 
     @Autowired
-    public CreditServiceImpl(CreditRepository creditRepository, CustomerController customerController, CreateCreditMapper createCreditMapper) {
+    public CreditServiceImpl(CreditRepository creditRepository, CustomerController customerController, CreateCreditMapper createCreditMapper, GetCreditsMapper getCreditsMapper) {
         this.creditRepository = creditRepository;
         this.customerController = customerController;
         this.createCreditMapper = createCreditMapper;
+        this.getCreditsMapper = getCreditsMapper;
     }
 
     @Override
     public CreditCreateResponse createCredit(CreateCreditRequest request) {
         Credit creditToSave;
         SearchCustomerRequest customer = createCreditMapper.mapCreateCreditRequestToSearchCustomerRequest(request);
-        CustomerSearchResponse customerFromRestCustomer = customerController.searchCustomer(customer);
-        if(customerFromRestCustomer.getSearchResult().isEmpty()){
+        CustomerSearchResponse findCustomer = customerController.searchCustomer(customer);
+        if(findCustomer.getSearchResult().isEmpty()){
             CreateCustomerResponse createCustomerResponse = customerController.createCustomer(
                    createCreditMapper.mapCreateCreditRequestToCreateCustomerRequest(request));
             creditToSave = createCreditMapper.mapCreateCreditResponseToEntity(request,createCustomerResponse);
         }else {
-            creditToSave = createCreditMapper.mapSearchCustomerResponseToEntity(request, customerFromRestCustomer);
+            creditToSave = createCreditMapper.mapSearchCustomerResponseToEntity(request, findCustomer);
         }
        Credit savedCredit = creditRepository.save(creditToSave);
         return createCreditMapper.mapEntityToCreditCreateResponse(savedCredit);
@@ -49,7 +53,9 @@ public class CreditServiceImpl implements CreditService {
 
     @Override
     public List<GetCreditsResponse> getAllCredits() {
-       //return creditRepository.findAll();
-        return null;
+      List<Credit> allCreditsFromDB = creditRepository.findAll();
+      GetCustomersResponse customers =customerController.getCustomers(getCreditsMapper.mapFromEntityTuGetCustomerRequest(allCreditsFromDB));
+        List<GetCreditsResponse> getCreditsResponse = getCreditsMapper.mapCreditAndCustomerToGetCreditResponse(allCreditsFromDB,customers);
+        return getCreditsResponse;
     }
 }
